1c77f99086868528622b5b840b5d72e74c21dce4
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-03-13 13:10:21 -0500

    adding first prob

--- sinisa-branch.js
+++ sinisa-branch.js
@@ -1,1 +1,14 @@
-//will upload problems upon completion
+//will upload problems upon completion!
+//Remove Trailing and Leading Zeros
+// Create a function that takes in a number as a string n and returns the number without trailing and leading zeros.
+//
+// Trailing Zeros are the zeros after a decimal point which don't affect the value (e.g. the last three zeros in 3.4000 and 3.04000).
+// Leading Zeros are the zeros before a whole number which don't affect the value (e.g. the first three zeros in 000234 and 000230).
+// Examples
+// removeLeadingTrailing("230.000") ➞ "230"
+//
+// removeLeadingTrailing("00402") ➞ "402"
+//
+// removeLeadingTrailing("03.1400") ➞ "3.14"
+//
+// removeLeadingTrailing("30") ➞ "30"
